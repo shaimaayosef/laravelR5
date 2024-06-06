692cac0470ec7acb9bf9400a341cb6075c999b79
shimaa sayed <shimaa.sayed8888@gmail.com>
2024-06-06 10:31:22 +0300

    day 10 -task

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->count(50)->create();
+    }
+}
